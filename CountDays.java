cebf0052e0df08f48695c5be11b87c1613fc487c
sushmija <113223920+sushmija@users.noreply.github.com>
2022-09-14 14:38:31 +0530

    Add files via upload

---
+++ CountDays.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class CountDays {
+    public static void main(String[] args)
+    {
+    	Scanner in = new Scanner(System.in);
+        System.out.println("Enter the days::\n");
+        int dd = in.nextInt();
+        int yy;
+        int ww;
+        yy = (dd / 365);
+        ww = (dd % 365) / 7;
+        dd = dd- ((yy * 365) + (ww));
+        System.out.println("\n" + yy + " Year, " + ww + " Weeks, and " + dd + " Days\n");
+    }
+}
