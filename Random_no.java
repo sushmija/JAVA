28aee7eb82cc83c8c8988e2955eb5735e71e3f02
sushmija <113223920+sushmija@users.noreply.github.com>
2022-10-14 09:19:22 +0530

    Add files via upload

---
+++ Random_no.java
@@ -0,0 +1,7 @@
+class Random_no{
+        public static void main(String[] args){
+            for(int i=1;i<=5;i++){
+                System.out.println((int)(Math.random()*100));
+            }
+        }
+    }
